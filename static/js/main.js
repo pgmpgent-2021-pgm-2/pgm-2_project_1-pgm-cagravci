b4ca68f9f66874b529786e73b1095d332fc151e0
pgm-cagravci <cagravci@student.arteveldehs.be>
2020-12-24 17:41:04 +0100

    fetches for weather, covid cases and pgm json is done

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,73 @@
 void (() => {
     const app = {
         initialize (){
-
+            this.cacheElements();
+            //fetch API data for covid cases
+            this.fetchGhentCovidPositiveCases();
+            //fetch API data for weather in city
+            this.fetchWeather();
+            //fetch API data for users
+            this.fetchUsers();
+        
+            this.registerListeners();
         },
         cacheElements () {
 
         },
         registerListeners () {
+
+        },
+        async fetchUsers() {
+            let pgmUsers = new UserApi();
+            pgmUsers.getUsers(users => {
+                console.log('pgm users are fetched: ', users);
+                this.updatePGMUsers(users);
+            });
+        },
+        updatePGMUsers(users) {
+            console.log('updating the pgm users has started...')
+           for (const key in users) {
+               console.log(users[key])
+           }
+
+        },
+        async fetchGhentCovidPositiveCases() {
+            let covidData = new GhentOpenDataApi();
+            covidData.getCovidPositiveCases(covidCases => {
+                console.log('covid cases are fetched: ', covidCases)
+                this.updateGhentCovidPositiveCases(covidCases)
+            })
+        },
+        updateGhentCovidPositiveCases(covidCases) {
+            console.log('updating the cases has started...')
+
+            document.querySelector('.nav__items').innerHTML += `
+            <li class="nav__item">
+                ${covidCases.records[0].fields.cases}
+            </li>`
+        },
+        async fetchWeather () {
+            let weather = new WeatherApi();
+            weather.getCurrentWeather('ghent', weather => {
+                console.log('weather is fetched: ', weather)
+                this.updateWeather(weather);
+            })
+        },
+        updateWeather (weather) {
+            console.log('updating the weather on page has started...')
+            document.querySelector('.nav__items').innerHTML += `
+            <li class="nav__item">
+                ${weather.location.name}
+            </li>
+            <li class="nav__item">
+                ${weather.current.condition.text}
+            </li>
+            <li class="nav__item">
+                <img src="${weather.current.condition.icon}" alt="weather-icon">
+            </li>
+            <li class="nav__item">
+                ${weather.current.temp_c}°C
+            </li>`
             
         }
     }
--- static/js/services.js
+++ static/js/services.js
@@ -5,23 +5,36 @@ function GitHubApi () {
 }
 
 function UserApi () {
-    this.getUsers = async () => {
-
-    }
+    this.getUsers = async (callback) => {
+        const HOSTNAME = "127.0.0.1";
+        const PORT = 5500;
+        const USERS_JSON_API = `http://${HOSTNAME}:${PORT}/static/data/pgm.json`;
+        fetch(USERS_JSON_API, {})
+                .then(response => response.json())
+                .then(json => callback(json))
+                .catch((error) => console.error(error));
+    }    
 }
 
 function WeatherApi () {
-    this.getCurrentWeather = async (city) => {
-        const WEATHER_API = '';
+    console.log('fetching weather has started...')
+    this.getCurrentWeather = async (city, callback) => {
+        console.log('weather for city is: ', city)
+        const WEATHER_API_KEY = 'fca0a22fc9a44098b85132431202412'
+        const WEATHER_API = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`;
         fetch(WEATHER_API, {})
                 .then(response => response.json())
-                .then(json => this.updateCountriesUI(json))
+                .then(json => callback(json))
                 .catch((error) => console.error(error));
-    }
+    }    
 }
 
 function GhentOpenDataApi () {
-    this.getCovidPositiveCases = async () => {
-
-    }
+    this.getCovidPositiveCases = async (callback) => {
+        const GHENT_COVID_API = `https://data.stad.gent/api/records/1.0/search/?dataset=dataset-of-cumulative-number-of-confirmed-cases-by-municipality&q=`;
+        fetch(GHENT_COVID_API, {})
+                .then(response => response.json())
+                .then(json => callback(json))
+                .catch((error) => console.error(error));
+    }    
 }
